92c2910f843e61c1c4ed54cb468e51f241cc8034
Ruben Horacio Marchiori <rubenmarchiori@gmail.com>
2024-08-01 13:23:05 -0300

    Router

--- defensa-civil/js/router.js
+++ defensa-civil/js/router.js
@@ -1,19 +1,36 @@
+
+
 const APP_PORT = 5500;
 
 
     // router.js
+    const route = (e) => {
+        e = e || window.e;
+        e.preventDefault();
+        window.history.pushState({}, '', e.target.href);
+    }
 
     // Objeto que almacena las rutas y sus correspondientes funciones
     const routes = {
-        '/': () => `http://localhost:${APP_PORT}/defensa-civil/index.html`,
-        '/eventos': () => `http://localhost:${APP_PORT}/defensa-civil/assets/sections/forms/control_panel.html`,
+        '/defensa-civil/': `http://localhost:${APP_PORT}/defensa-civil/eventos/main.html`,
+        '/defensa-civil/eventos': `http://localhost:${APP_PORT}/defensa-civil/eventos/control_panel.html`,
     };
 
     // Función que renderiza el contenido según la ruta
-    const router = () => {
-        const path = location.hash.slice(1) || '/';
-        const render = routes[path] || (() => `<h1>404</h1><p>Página no encontrada.</p>`);
-        href = render();
+    const router = async () => {
+        //const path = location.hash.slice(1) || '/';
+        const path = window.location.pathname;
+        
+        console.log(path);
+        const route = routes[path];
+        console.log(route);
+        try {
+            const html = await fetch(route).then(data => data.text());
+            document.getElementById('main-page').innerHTML = html
+        } catch (error) {
+            document.getElementById('main-page').innerHTML = `<h1>404</h1><p>Página no encontrada.</p>`;
+            console.error(error);
+        }
     };
 
     // Evento que escucha los cambios en el hash de la URL
@@ -22,4 +39,4 @@ const APP_PORT = 5500;
     // Llamar al router para cargar la ruta inicial
     window.addEventListener('load', router);
 
-
+    window.route = route;
